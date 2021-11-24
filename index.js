c3ccbbe44321165b018130bd163fa03319823562
wilsongoltara <wilson.goltara15@gmail.com>
2021-11-24 15:27:16 -0300

    create index and app

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const routes = require('./config/routes')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(routes)
+
+app.listen(3000, () => {
+    console.log("Express start at http://localhost:3000")
+})
