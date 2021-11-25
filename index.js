b4b3dcf5f1d339baa47540d6d80e20c063eaac36
wilsongoltara <wilson.goltara15@gmail.com>
2021-11-25 15:42:32 -0300

    formatação no código

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const routes = require('./config/routes')
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./config/routes');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(routes)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(routes);
 
 app.listen(3000, () => {
-    console.log("Express start at http://localhost:3000")
-})
+    console.log("Express start at http://localhost:3000");
+});
