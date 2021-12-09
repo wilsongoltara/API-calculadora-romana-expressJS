e127ba8503ecddf2a29feec41ef19aeda0a82d45
Wilson da Silva Goltara <wilson.goltara15@gmail.com>
2021-12-08 21:48:10 -0300

    refactoring code

--- config/routes.js
+++ config/routes.js
@@ -1,133 +1,136 @@
-const express = require('express')
-const routes = express.Router()
+const express = require('express');
+const routes = express.Router();
 
 //Converte para Algarismos romanos.
-const convertToRoman = (num) => {
+const converteRomano = (num) => {
     const roman = 
     {
-      M: 1000,
-      CM: 900,
-      D: 500,
-      CD: 400,
-      C: 100,
-      XC: 90,
-      L: 50,
-      XL: 40,
-      X: 10,
-      IX: 9,
-      V: 5,
-      IV: 4,
-      I: 1
-    }
-    let str = ''
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    let str = '';
   
-    for (let i of Object.keys(roman)) 
+    for(let i of Object.keys(roman)) 
     {
-        let q = Math.floor(num / roman[i])
-        num -= q * roman[i]
-        str += i.repeat(q)
+        let q = Math.floor(num / roman[i]);
+        num -= q * roman[i];
+        str += i.repeat(q);
     } 
-    return str
+    return str;
 }
 
 //Soma números recebidos
 const somaRomano = (valores) => {
-	let acumulador = 0
+	let acumulador = 0;
 	let valoresEmDecimal = valores;
 
 	for(i = 0; i < valoresEmDecimal.length; i++) 
     {
-		acumulador += valoresEmDecimal[i]
+		acumulador += valoresEmDecimal[i];
 	}
-    console.log(acumulador)
-	return acumulador
+    console.log(acumulador);
+	return acumulador;
 }
 
 //Subtraí números recebidos
 const subtracaoRomano = (valores) => {
-	let aux = valores[0] 
-	let valoresEmDecimal = valores
-    let sub = 0
+	let aux = valores[0];
+	let valoresEmDecimal = valores;
+    let sub = 0;
+
 	for(i = 1; i < valoresEmDecimal.length; i++)
     {
-		sub = aux - valoresEmDecimal[i]
-        aux = sub
+		sub = aux - valoresEmDecimal[i];
+        aux = sub;
 	}
-	return sub
+	return sub;
 }
 
 //Aviso erro ao entrar na pasta raiz
 routes.get('/', (req, res) => { 
-    res.status(404).send("Not Found")
+    res.status(404).send("Not Found");
 })
 
 //Responde com um mensagem sobre o que é a API
 routes.get('/about', (req, res) => {
     res.send({
         msg:"API Calculadora romana"
-    })
+    });
 })
 
 //Responde com uma menssagem. 
 routes.get('/calculadora', (req, res) => {
-    res.send("Calculadora Romana")
+    res.send("Calculadora Romana");
 })
 
 //Chama funcao somaRomando passando valores recebidos
 // e retorna soma em algarismo arábico e romano
 routes.get('/calculadora/soma/', (req, res) => {
-    const body = req.body
-    const soma = somaRomano(body)
+    const body = req.body;
+    const soma = somaRomano(body);
     res.send({"soma": soma,
-            "soma-romana:":convertToRoman(soma)})
+            "soma-romana:":converteRomano(soma)
+    });
 })
 
 //Chama funcao subtracaoRomando passando valores recebidos
 // e retorna soma em algarismo arábico e romano
 routes.get('/calculadora/subtracao', (req, res) => {
-    const body = req.body
-    const sub = Math.abs(subtracaoRomano(body))
+    const body = req.body;
+    const sub = Math.abs(subtracaoRomano(body));
     res.send({"subtracao": sub,
-            "sub-romana:":convertToRoman(sub)})
+            "sub-romana:":converteRomano(sub)
+    });
 })
 
 //Retorna json com os números e seus respectivos algarismos.
 routes.get('/calculadora/numeros', (req, res) => {
-    const body = req.body
-    let algarismos = [], i
+    const body = req.body;
+    let algarismos = [], i;
 
-    for(i  in body)
+    for(i in body)
     {
-      algarismos.push(convertToRoman(body[i]))
+      algarismos.push(converteRomano(body[i]))
     }
-    res.json({"Algarismos": algarismos,"Números": body})
+    res.json({"Algarismos": algarismos,"Números": body});;
 })
 
 //Retorna a operacao feita na somaRomano
 routes.get('/calculadora/operacao/soma', (req, res) => {
-    const body = req.body
-    let algarismos = [], i
-    let soma = convertToRoman(somaRomano(body))
-    let result = ``
+    const body = req.body;
+    let algarismos = [], i;
+    let soma = converteRomano(somaRomano(body));
+    let result = ``;
     
     for(i in body)
     {
-        algarismos.push(convertToRoman(body[i]))
-        result += `${algarismos[i]} + `
+        algarismos.push(converteRomano(body[i]));
+        result += `${algarismos[i]} + `;
     }
-    res.send(`${result}` + `= ${soma}`)
+    res.send(`${result}` + `= ${soma}`);
 })
 //Retorna a operacao feita na subtracaoRomano
 routes.get('/calculadora/operacao/subtracao', (req, res) => {
-    const body = req.body
-    let algarismos = [], i
-    let sub = Math.abs(subtracaoRomano(body))
-    let result = ``
-    sub = convertToRoman(sub)
+    const body = req.body;
+    let algarismos = [], i;
+    let sub = Math.abs(subtracaoRomano(body));
+    let result = ``;
+    sub = converteRomano(sub);
       
     for(i in body)
     {
-        algarismos.push(convertToRoman(body[i]))
+        algarismos.push(converteRomano(body[i]))
         result += `${algarismos[i]} - `
     }
     res.send(`${result}` + `= ${sub}`)
