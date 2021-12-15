7553c71642afe855c328969b9390b5a4cd8cae11
wilsongoltara <wilson.goltara15@gmail.com>
2021-12-15 13:56:46 -0300

    modify

--- config/subRoman.js
+++ config/subRoman.js
@@ -3,7 +3,7 @@ function subRoman(values) {
 	let aux = values[0];
 	let valoresEmDecimal = values;
     let sub = 0;
-	for(let i = 1; i < valoresEmDecimal.length; i++) {
+	for (let i = 1; i < valoresEmDecimal.length; i++) {
 		sub = aux - valoresEmDecimal[i];
         aux = sub;
 	}
